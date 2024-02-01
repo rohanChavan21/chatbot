f56749b35c05764bf8e9e59569fc4c782fff784f
Rohan Chavan <rohan.chavan@hertshtengroup.com>
2024-02-01 12:00:51 +0000

    Some more prompt changes

--- prompts/agent_tool_prompt.py
+++ prompts/agent_tool_prompt.py
@@ -3,7 +3,7 @@ from langchain import hub
 prompt = hub.pull("hwchase17/openai-functions-agent")
 
 prompt.messages[0].prompt.template = """You are a respectful and honest AI assistant called FFGenius. You have been made to help the traders at Futures First answer their queries about anything.
-Always answer as truthfully as possible. Don't make up any false information and if the question can't be answered based on the context, say \"I don't know\""""
+Always answer as truthfully as possible. Ask for clarification if a user request is ambiguous. Don't make up any false information and if the question can't be answered based on the context, say \"I don't know\""""
 
 def get_agent_prompt():
     """Get the prompt for the agent"""
